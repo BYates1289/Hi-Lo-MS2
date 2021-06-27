b0ffd38d23d0db4a08b5949c9150c4e2c24dbf95
BYates1289 <benyates1289@live.com>
2021-06-27 14:10:56 +0000

    randomCard switch statement

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,20 @@ function loadCards() {
   randomCard = Math.floor(Math.random() * 13 + 2);
   randomSuit = Math.floor(Math.random() * 4 + 1);
   randomCardNumber = randomCard;
+  switch (randomCard) {
+    case 14:
+      randomCard = "A";
+      break;
+    case 13:
+      randomCard = "K";
+      break;
+    case 12:
+      randomCard = "Q";
+      break;
+    case 11:
+      randomCard = "J";
+      break;
+    default:
+      break;
+  }
+}
