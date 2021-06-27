29ee85c8cfa67d284b964307f7a0e6b710d0738d
BYates1289 <benyates1289@live.com>
2021-06-27 10:26:19 +0000

    Added loadCards function

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,7 @@
 const card = document.getElementById("card");
 const lastCard = document.getElementById("lastCard");
+
+function loadCards() {
+  randomCard = Math.floor(Math.random() * 13 + 2);
+  randomSuit = Math.floor(Math.random() * 4 + 1);
+  randomCardNumber = randomCard;
